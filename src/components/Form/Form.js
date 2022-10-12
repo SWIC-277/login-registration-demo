cf9f8101c220cc35577305b6aacb8d90f3714153
Manav Misra <manavm@visionwebsoft.com>
2022-10-12 18:50:35 -0500

    Add in-class comments

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,10 @@
 import Button from "./Button";
 import Input from "./Input";
 
+// We map over the Array of strings and return an Input component for each one
 export default ({ fields, button }) => `
   <form class="mx-auto mt-2 max-w-xs">
-    ${fields.map((input) => Input({ label: input })).join("")}
-    ${Button({ ...button })}
+    ${fields.map((field) => Input({ label: field })).join("\n")}
+    ${Button(button)}
   </form>
 `;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import Header from "./components/Header";
 
 const root = document.getElementById("root");
 
+// There is usually an App component which serves as a top level component
 const App = () => {
+  // State represents all of the 'moving parts' of the application
+  // State is an object that contains all of the data that the application needs
+  // State will be updated based on user input and/or data from the server
   const state = {
     title: "My Form",
     fields: ["email", "password"],
@@ -13,12 +17,15 @@ const App = () => {
     },
   };
 
+  // We can pass state from a parent component to a child component
+  // State can be passed as props
   return `
     ${Header(state)}
     ${Form(state)}
   `;
 };
 
+// Render the App component to the DOM
 function render() {
   root.innerHTML = App();
 }
